763de0af2e83151bd09aea07b73b3636206cf319
LoriSun <6099012@qq.com>
2022-10-21 15:33:10 +0800

    keep screen unloocked

--- core/processors/MOUSE_CLICKProcessor.py
+++ core/processors/MOUSE_CLICKProcessor.py
@@ -12,7 +12,7 @@ pip install pyautogui
 
 
 class MOUSE_CLICKProcessor(Processor):
-    TPL: str = '{"x":10, "y":10, "wait":5}'
+    TPL: str = '{"x_from":"mouse_at_x","y_from":"mouse_at_y", "x":-1, "y":-1, "wait":5}'
 
     DESC: str = f'''
         To support mouse click on position(x,y)
@@ -20,9 +20,16 @@ class MOUSE_CLICKProcessor(Processor):
     '''
 
     def process(self):
+
         x = self.get_param('x')
         y = self.get_param('y')
+
+        if (x == -1 and y == -1) or (x is None and y is None):
+            x_at = self.get_data(self.get_param('x_from'))
+            y_at = self.get_data(self.get_param('y_from'))
+            pyautogui.click(x=x_at, y=y_at)
+        else:
+            pyautogui.click(x=x, y=y)
+
         wait = self.get_param('wait')
         time.sleep(wait)
-
-        pyautogui.click(x=x, y=y)
---
+++ core/processors/MOUSE_POSITIONProcessor.py
@@ -0,0 +1,36 @@
+import time
+
+import logging
+
+import pyautogui
+
+from core.processor import Processor
+
+"""
+
+pip install pyautogui
+
+"""
+
+
+class MOUSE_POSITIONProcessor(Processor):
+    TPL: str = '{"data_key_x":"mouse_at_x", "data_key_y":"mouse_at_y", "wait":5}'
+
+    DESC: str = f'''
+        To get mouse position(x,y), then bind to data_key_x and y
+        {TPL}
+    '''
+
+    def process(self):
+        wait = self.get_param('wait')
+        if not wait is None and wait > 0:
+            time.sleep(wait)
+
+        data_key_x = self.get_param('data_key_x')
+        data_key_y = self.get_param('data_key_y')
+
+        position = pyautogui.position()
+
+        self.populate_data(data_key_x, position.x)
+        self.populate_data(data_key_y, position.y)
+        logging.info(f'current mouse at [{position.x},{position.y}]')
