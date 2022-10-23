b63d929c41db3b27a1ada29ce64b771398222377
LoriSun <6099012@qq.com>
2022-10-23 12:55:09 +0800

    keep screen unloocked

--- core/processors/MOUSE_SCROLLProcessor.py
+++ core/processors/MOUSE_SCROLLProcessor.py
@@ -11,7 +11,7 @@ pip install pyautogui
 
 
 class MOUSE_SCROLLProcessor(Processor):
-    TPL: str = '{"x":10, "y":10, "vertical":-10, "wait":5}'
+    TPL: str = '{"x_from":"mouse_at_x","y_from":"mouse_at_y", "x":-1, "y":-1, "vertical":10, "wait":5}'
 
     DESC: str = f'''
         To support mouse scroll on position(x,y), vertical
@@ -22,7 +22,13 @@ class MOUSE_SCROLLProcessor(Processor):
         x = self.get_param('x')
         y = self.get_param('y')
         vertical = self.get_param('vertical')
-        wait = self.get_param('wait')
+        
+        if (x == -1 and y == -1) or (x is None and y is None):
+            x_at = self.get_data(self.get_param('x_from'))
+            y_at = self.get_data(self.get_param('y_from'))
+            pyautogui.scroll(vertical, x=x_at, y=y_at)
+        else:
+            pyautogui.scroll(vertical, x=x, y=y)
 
+        wait = self.get_param('wait')
         time.sleep(wait)
-        pyautogui.scroll(vertical, x=x, y=y)
