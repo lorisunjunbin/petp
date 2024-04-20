e0bad62b2c5ca000c46bf4f962a493221dfbf044
CodingCat <sunjunbin@gmail.com>
2024-04-20 10:48:22 +0800

    clean code

--- PETP_build.py
+++ PETP_build.py
@@ -20,23 +20,32 @@ def copy_folder_to_dist(directories):
         shutil.copytree(source, destination, dirs_exist_ok=True)
 
 
-if __name__ == '__main__':
-    # clean dist folder
+def clean_dist():
     shutil.rmtree('dist', ignore_errors=True)
     os.makedirs('dist', exist_ok=True)
 
-    # figure out all the necessary python files,  make them as hidden imports
+
+def collect_hidden_imports():
     loaded_files = glob.glob(os.path.join('utils', '**/*.py'), recursive=True)
     loaded_imports = [f.replace('\\', '.').replace('/', '.').rstrip('.py') for f in loaded_files]
     _hiddenimports = ['utils']
-    for utils in loaded_imports:
-        _hiddenimports.append(utils)
+    for hidden_import in loaded_imports:
+        _hiddenimports.append(hidden_import)
 
     replace_words_in_file('PETP.spec', '$hidden_imports$', str(_hiddenimports))
 
+
+if __name__ == '__main__':
+    # clean dist folder
+    clean_dist()
+
+    # figure out all the necessary python files,  make them as hidden imports
+    collect_hidden_imports()
+
     # move config,core, image, testcoverage to dist folder.
     copy_folder_to_dist(
-        ['config', 'core/executions', 'core/processors', 'core/pipelines', 'image', 'resources', 'testcoverage'])
+        ['config', 'core/executions', 'core/processors', 'core/pipelines', 'image', 'resources', 'testcoverage']
+    )
 
     # run pyinstaller
     subprocess.run(['pyinstaller', './PETP.spec'], check=True)
